8b1f62749846b01a643b51026615fb82e3263435
WeiChiaChang <ioh04019@gmail.com>
2017-09-11 18:28:43 +0800

    ✨ add ghost.gif

--- scripts/easter-egg-collection.js
+++ scripts/easter-egg-collection.js
@@ -1502,6 +1502,48 @@
     };
   
     var data = document.getElementById('gif-pikachu').dataset.src
-    init(data)
-})(window)
+    init(data);
+})(window);
 // GIF -- pikachu -- end
+
+// GIF -- ghost -- start
+(function(window){
+  
+    "use strict";  
+    var ghost = function() {
+      var shock = document.createElement('div');
+      var img = new Image();
+      img.src = data;
+      img.style.width = '374px';
+      img.style.height = '375px';
+      img.style.transition = '6s all';
+      img.style.position = 'fixed';
+      img.style.right = '-374px';
+      img.style.bottom = '0px';
+      img.style.zIndex = 999999;
+  
+      document.body.appendChild(img);
+  
+      window.setTimeout(function(){
+        img.style.right = 'calc(50% - 187px)';
+      },50);
+  
+      window.setTimeout(function(){
+        img.style.right = 'calc(100% + 375px)';
+      }, 4300);
+      window.setTimeout(function(){
+        img.parentNode.removeChild(img);
+      }, 7300);
+  
+    };
+
+    var init = function(data) {
+      var qwop = document.getElementById('gif-ghost');
+      qwop.addEventListener('click', ghost)
+    };
+  
+    var data = document.getElementById('gif-ghost').dataset.src
+    init(data)
+
+  })(window);
+  // GIF -- ghost -- end
