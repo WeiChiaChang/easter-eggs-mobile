e27df9054855bab198893c2220fa2bbb19bbaf54
WeiChiaChang <ioh04019@gmail.com>
2017-09-13 11:41:18 +0800

    ✨ add salamander.gif

--- scripts/easter-egg-mobile.js
+++ scripts/easter-egg-mobile.js
@@ -1736,3 +1736,55 @@
 
 })(window);
 // GIF -- psyduck -- end
+
+// GIF -- salamander -- start
+(function (window) {
+  "use strict"
+
+  var salamander = function () {
+
+    var shock = document.createElement('div');
+    var img = new Image();
+    img.src = data;
+    img.style.width = '374px';
+    img.style.height = '375px';
+    img.style.transition = '13s all';
+    img.style.position = 'fixed';
+    img.style.right = '-374px';
+    img.style.bottom = '0px';
+    img.style.zIndex = 999999;
+
+    document.body.appendChild(img);
+
+    window.setTimeout(function () {
+      img.style.right = 'calc(100% + 500px)';
+    }, 50);
+
+    // window.setTimeout(function(){
+    //   img.style.right = 'calc(100% + 375px)';
+    // }, 4500);
+
+    window.setTimeout(function () {
+      img.parentNode.removeChild(img);
+    }, 10300);
+
+  };
+
+  var init = function (data) {
+    try {
+      var qwop = document.getElementsByClassName('gif-salamander')[0];
+      qwop.addEventListener('click', salamander)
+    } catch (e) {
+      // console.log("error : "+e);
+    }
+  };
+
+  try {
+    var data = document.getElementsByClassName('gif-salamander')[0].dataset.src
+    init(data);
+  } catch (e) {
+    // console.log("error : "+e);
+  }
+
+})(window);
+// GIF -- salamander -- end
